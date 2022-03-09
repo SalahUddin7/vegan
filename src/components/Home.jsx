dcdb5cfea03fff15eff2d61e99dfd523a2f6b070
Salah Uddin <ziko.salahuddin@gmail.com>
2022-03-09 16:28:22 +0100

    css edited for flexbox

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,36 @@ import HeroSection from './HeroSection';
 const Home = () => {
   return (
     <div>
-      <div>
-        <HeroSection />
+      <HeroSection />
+
+      <div className="HomePage">
+        <div className="flex-containerA">
+          <div className="flex-item-left">
+            <img src="veganhero.jpg" alt="" />
+          </div>
+          <div className="flex-item-right">
+            Text A Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Libero illo, fugit eos blanditiis et doloribus deleniti rerum nam
+            qui vero ipsum consequuntur ut dolor nesciunt porro, autem omnis!
+          </div>
+        </div>
+
+        <div className="flex-containerB">
+          <dir>Text B</dir>
+          <div>
+            <img src="veganhero.jpg" alt="" />
+          </div>
+        </div>
+
+        <div className="flex-containerA">
+          <div>
+            <img src="veganhero.jpg" alt="" />
+          </div>
+          <dir>Text C</dir>
+        </div>
       </div>
       <div>
-        <h2>This is Home Page d</h2>
+        <h2>This is Home Page e</h2>
 
         <h2>
           This is Dish Product
