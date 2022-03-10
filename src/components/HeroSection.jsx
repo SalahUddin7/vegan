0c7cc894aca0cd13de7695a08e78a73835f1a52b
Salah Uddin <ziko.salahuddin@gmail.com>
2022-03-10 14:02:58 +0100

    Main Page Section CSS edited

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <div className="hero-section">
       <section>
-        <img src="veganhero.jpg" alt="" />
+        <img src="veganhero2.jpg" alt="" />
       </section>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,18 +13,30 @@ const Home = () => {
           <div className="flex-item-left">
             <img src="Homepage-images/main-dish.jpg" alt="" />
           </div>
-          <div className="flex-item-right">
-            Text A dolor nesciunt porro, autem omnis! Text A dolor nesciunt
-            porro, autem omnis! Text A dolor nesciunt porro, autem omnis!
+          <div className="flex-item-right main-page--section-text">
+            <div>
+              {' '}
+              Text A dolor nesciunt porro, autem omnis! Text A dolor nesciunt
+              porro, autem omnis! Text A dolor nesciunt porro, autem omnis!
+            </div>
+            <div>
+              {' '}
+              <button className="btn">
+                <Link to="/products">View Main Dish Product. </Link>
+              </button>
+            </div>
           </div>
         </section>
 
         {/* CONTAINER B: MAIN DESSERTS */}
         <section className="flex-containerA">
-          <div className="flex-item-right">
+          <div className="flex-item-right main-page--section-text">
             Text B: Text A dolor nesciunt porro, autem omnis! Text A dolor
             nesciunt porro, autem omnis! Text A dolor nesciunt porro, autem
             omnis!
+            <button>
+              <Link to="/DessertProduct"> View Dessert Items</Link>
+            </button>
           </div>
           <div className="flex-item-left">
             <img src="Homepage-images/main-desserts.jpg" alt="" />
@@ -36,32 +48,18 @@ const Home = () => {
           <div className="flex-item-left">
             <img src="Homepage-images/main-drinks.jpg" alt="" />
           </div>
-          <div className="flex-item-right">
+          <div className="flex-item-right main-page--section-text">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Exercitationem cum dolorem ratione, ipsam architecto qui iste facere
             aspernatur doloremque modi dolore atque, est dolorum nemo nostrum
             quaerat sapiente laudantium consequuntur!
+            <div>
+              <button>
+                <Link to="/DrinksProduct"> View Drinks Items</Link>
+              </button>
+            </div>
           </div>
         </section>
-      </div>
-      <div>
-        <h2>This is Home Page e</h2>
-
-        <h2>
-          This is Dish Product
-          <button>
-            {' '}
-            <Link to="/products">View Main Dish Product</Link>
-          </button>
-        </h2>
-
-        <h2>
-          This is Dessert Product
-          <button>
-            {' '}
-            <Link to="/DessertProduct"> View Dessert Items</Link>
-          </button>
-        </h2>
       </div>
     </div>
   );
